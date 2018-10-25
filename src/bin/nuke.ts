cffac11c0735c6e0c9354483e6cc933bc541ca75
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-25 23:00:38 +0100

    added shebang, sorted imports

--- src/bin/nuke.ts
+++ src/bin/nuke.ts
@@ -1,17 +1,15 @@
+#!/usr/bin/env node
+
 import { Errors } from "../system/errors";
 import { nuke } from "../system/fs/nuke";
-import {
-  argv,
-  error_logger,
-  formatError
-} from "./context";
+import * as context from "./context";
 
-for (const dir of argv._.slice(2)) {
+for (const dir of context.argv._.slice(2)) {
   nuke(dir)
       .then(() => console.info(`Nuked ${dir}!`))
       .catch((err) => {
         const info = Errors.get(err.code);
-        const text = formatError(info.message);
-        error_logger.error(`${text}`);
+        const text = context.formatError(info.message);
+        context.error_logger.error(`${text}`);
       });
 }
