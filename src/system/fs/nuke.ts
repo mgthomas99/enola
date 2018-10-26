2ec2673eb5319cc92ddc25b1d2d87ec11b70297c
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-26 03:18:04 +0100

    sync function

--- src/system/fs/nuke.ts
+++ src/system/fs/nuke.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as path from "path";
 
 /**
  * Obliterates the file/directory at the specified path.
@@ -8,19 +9,36 @@ import * as fs from "fs";
  * @return  {Promise<void>}
  *          A `Promise` which is resolved once the item has been destroyed.
  */
-export function nuke(path: string)
+export function nuke(dir: string)
 : Promise<void> {
   return new Promise((accept, reject) => {
-    fs.stat(path, (err, stats) => {
-      if (err) return reject(err);
-      if (stats.isFile()) {
-        //
+    fs.stat(dir, (err, stats) => {
+      if (err) {
+        reject(err);
+      } else if (stats.isFile()) {
+        fs.unlink(dir, (err2) => {
+          if (err2) {
+            reject(err2);
+          } else {
+            accept();
+          }
+        });
       } else if (stats.isDirectory()) {
-        fs.readdir(path, (err2, files) => {
+        fs.readdir(dir, (err2, files) => {
           if (err2) return reject(err2);
-          const cbs = files.map(x => nuke(x));
+          const cbs = files
+              .map(x => path.join(dir, x))
+              .map(x => nuke(x));
           Promise.all(cbs)
-              .then(() => void accept())
+              .then(() => {
+                fs.rmdir(dir, (err4) => {
+                  if (err4) {
+                    reject(err4);
+                  } else {
+                    accept();
+                  }
+                });
+              })
               .catch((err3) => void reject(err3));
         });
       } else {
@@ -28,4 +46,24 @@ export function nuke(path: string)
       }
     });
   });
+}
+
+/**
+ * @param   {string}  dir
+ *          Path to a file or directory to destroy.
+ * @return  {void}
+ * @throws  {Error}
+ */
+export function nukeSync(dir: string)
+: never | void {
+  const stats = fs.statSync(dir);
+  if (stats.isDirectory()) {
+    const files = fs.readdirSync(dir);
+    files
+        .map(x => path.join(dir, x))
+        .map(x => nukeSync(x));
+    fs.rmdirSync(dir);
+  } else if (stats.isFile()) {
+    fs.unlinkSync(dir);
+  }
 }
