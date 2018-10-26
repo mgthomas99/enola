8934f35f16372d0c39645a2ea85302fdc25009a8
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-26 03:18:14 +0100

    use `nukeSync`

--- src/bin/nuke.ts
+++ src/bin/nuke.ts
@@ -1,15 +1,8 @@
 #!/usr/bin/env node
 
-import { Errors } from "../system/errors";
-import { nuke } from "../system/fs/nuke";
+import { nukeSync } from "../system/fs/nuke";
 import * as context from "./context";
 
 for (const dir of context.argv._.slice(2)) {
-  nuke(dir)
-      .then(() => console.info(`Nuked ${dir}!`))
-      .catch((err) => {
-        const info = Errors.get(err.code);
-        const text = context.formatError(info.message);
-        context.error_logger.error(`${text}`);
-      });
+  nukeSync(dir);
 }
