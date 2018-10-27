40adbd304806def3dc1504bcea1971d13bc33fbb
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-27 03:20:48 +0100

    expanded cli functionality

--- src/bin/nuke.ts
+++ src/bin/nuke.ts
@@ -1,8 +1,31 @@
 #!/usr/bin/env node
 
-import { nukeSync } from "../system/fs/nuke";
-import * as context from "./index";
+import * as yargs from "yargs";
 
-for (const dir of context.argv._.slice(2)) {
-  nukeSync(dir);
-}
+import { nuke } from "../system/fs/nuke";
+import { explode } from "../explode";
+import * as index from "./index";
+
+const argv = yargs
+    .boolean("pretty")
+      .alias("pretty", "p")
+      .default("pretty", true)
+      .describe("pretty", "Output styling")
+    .boolean("silent")
+      .alias("silent", "s")
+      .default("silent", false)
+      .describe("silent", "Mute output")
+    .parse(process.argv);
+
+const paths = argv._.slice(2);
+const promises = paths.map(x => nuke(x));
+const start = process.hrtime();
+Promise.all(promises)
+    .then(() => {
+      const elapsed = process.hrtime(start);
+      const seconds = elapsed[0] + elapsed[1] / 1e9;
+
+      explode();
+      console.log(`Took ${seconds} seconds!`);
+    })
+    .catch((err) => index.logger.error(err));
