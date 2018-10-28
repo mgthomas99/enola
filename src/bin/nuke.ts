475c9a98ec160f78a42b2b52379f4765001c5e6a
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-28 01:11:43 +0100

    nicer output formatting

--- src/bin/nuke.ts
+++ src/bin/nuke.ts
@@ -6,6 +6,24 @@ import { nuke } from "../system/fs/nuke";
 import { explode } from "../explode";
 import * as index from "./index";
 
+function cbify(dir: string)
+: (Promise<{
+  elapsed: number;
+  path: string;
+}>) {
+  const a = process.hrtime();
+
+  return nuke(dir).then(() => {
+    const elapsed = process.hrtime(a);
+    const seconds = elapsed[0] + elapsed[1] * 1e-12;
+
+    return ({
+      elapsed: seconds,
+      path: dir
+    });
+  });
+}
+
 const argv = yargs
     .boolean("pretty")
       .alias("pretty", "p")
@@ -17,15 +35,16 @@ const argv = yargs
       .describe("silent", "Mute output")
     .parse(process.argv);
 
-const paths = argv._.slice(2);
-const promises = paths.map(x => nuke(x));
-const start = process.hrtime();
-Promise.all(promises)
-    .then(() => {
-      const elapsed = process.hrtime(start);
-      const seconds = elapsed[0] + elapsed[1] / 1e9;
+const promises = argv._.slice(2)
+    .map(index.resolvePath)
+    .map(cbify);
 
+Promise.all(promises)
+    .then((values) => {
       explode();
-      console.log(`Took ${seconds} seconds!`);
+
+      values.forEach((x) => {
+        console.log(`Nuked ${x.path} in ${x.elapsed.toFixed(12)} seconds!`);
+      });
     })
     .catch((err) => index.logger.error(err));
