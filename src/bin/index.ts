4650b9fbd2be2ea515003cb81d0f87bddc3db599
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-28 01:11:27 +0100

    added `resolvePath`

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,16 +1,23 @@
 import * as log4js from "log4js";
+import * as path from "path";
 import chalk from "chalk";
 
 export function formatError(err: Error | string)
-: string {
+: (string) {
   const prefix = chalk.bgYellow("Enola");
   const type = chalk.redBright("ERR");
   const content = getErrorMessage(err);
   return `${prefix} ${type} ${content}`;
 }
 
+export function resolvePath(dir: string)
+: (string) {
+  const cwd = process.cwd();
+  return path.join(cwd, dir);
+}
+
 export function getErrorMessage(err: Error | string)
-: string {
+: (string) {
   if (err instanceof Error) {
     return err.message;
   }
