1e32f358963b20009872c2a96103e1afba6ec6e0
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-29 21:11:53 +0000

    sync function

--- src/system/fs/stats.ts
+++ src/system/fs/stats.ts
@@ -1,7 +1,17 @@
 import * as fs from "fs-extra";
 
+export function statSyncSafe(dir: string)
+: (fs.Stats | undefined) {
+  try {
+    const stat = fs.statSync(dir);
+    return stat;
+  } catch (ex) {
+    return undefined;
+  }
+}
+
 export function statSafe(dir: string)
-: (Promise<fs.Stats>) {
+: (Promise<fs.Stats | undefined>) {
   return new Promise((accept, reject) => {
     fs.stat(dir, (err, stats) => {
       if (err) accept(undefined);
