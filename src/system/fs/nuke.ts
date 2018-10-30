0308a00c763d336f0286cc6f9016c81ae57909f6
gthomas <gthomas@geologging.com>
2018-10-30 16:20:46 +0000

    return info about the operation

--- src/system/fs/nuke.ts
+++ src/system/fs/nuke.ts
@@ -3,6 +3,16 @@ import * as path from "path";
 
 import * as stat from "./stat";
 
+export type NukeResult = ({
+  children: NukeResult[];
+  path: string;
+  err?: {
+    errno: number;
+    code: string;
+    message?: string;
+  };
+});
+
 /**
  * Asynchronously obliterate the file/directory at the specified path.
  *
@@ -15,27 +25,38 @@ import * as stat from "./stat";
  *          Optional `boolean` parameter. If `false`, the function will return
  *          immediately. This parameter is automatically provided internally
  *          when this function is called recursively.
- * @return  {Promise<void>}
+ * @return  {Promise<NukeResult>}
  *          A `Promise` which is resolved once the item has been destroyed, or
  *          rejected if a system error occurs.
  */
 export async function nuke(dir: string)
-: (Promise<void>) {
+: (Promise<NukeResult>) {
   const stats = await stat.statSafe(dir);
-  if (typeof stats === "undefined") {
-    return;
-  }
+  const result: NukeResult = ({
+    children: [],
+    path: dir
+  });
 
-  if (stats.isDirectory()) {
-    const files = await fs.readdir(dir);
+  if (typeof stats === "undefined") {
+    result.err = ({
+      errno: 1,
+      code: "ENOTEXIST",
+      message: `'${dir}' does not exist. Skipping!`
+    });
+  } else if (stats.isDirectory()) {
+    const files = (await fs.readdir(dir))
+        .map(x => path.join(dir, x));
     const callbacks = files
-        .map(x => path.join(dir, x))
         .map(x => nuke(x));
-    await Promise.all(callbacks);
+
+    const children = await Promise.all(callbacks);
+    result.children.push(... children);
+
     await fs.rmdir(dir);
   } else if (stats.isFile()) {
     await fs.unlink(dir);
   }
+  return result;
 }
 
 /**
@@ -46,23 +67,30 @@ export async function nuke(dir: string)
  * @param   {string}  dir
  *          The path of the file/directory to destroy. The path can either be
  *          absolute, or relative to the current working directory.
- * @return  {void}
+ * @return  {NukeResult}
  * @throws  {Error}
  */
 export function nukeSync(dir: string)
-: (never | void) {
+: (NukeResult | never) {
   const stats = stat.statSyncSafe(dir);
-  if  (typeof stats === "undefined") {
-    return;
-  }
+  const result: NukeResult = ({
+    children: [],
+    path: dir
+  });
 
-  if (stats.isDirectory()) {
+  if  (typeof stats === "undefined") {
+
+  } else if (stats.isDirectory()) {
     const files = fs.readdirSync(dir);
     files
         .map(x => path.join(dir, x))
-        .map(x => nukeSync(x));
+        .map(x => nukeSync(x))
+        .forEach((x) => {
+          result.children.push(x);
+        });
     fs.rmdirSync(dir);
   } else if (stats.isFile()) {
     fs.unlinkSync(dir);
   }
+  return result;
 }
