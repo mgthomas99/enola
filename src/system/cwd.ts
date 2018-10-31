1f172ca2137268bdb5ae5833120fe8e7033a37a0
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-31 00:38:51 +0000

    doc

--- src/system/cwd.ts
+++ src/system/cwd.ts
@@ -1,9 +1,18 @@
 import * as path from "path";
 
-export function cwd(dir?: string)
+/**
+ * Join all arguments using the operating system's path separator, and append
+ * the result to the current working directory.
+ *
+ * @param   {string}  dir
+ *          The strings to join using the operating system's path separator.
+ * @return  {string}
+ *          The path.
+ */
+export function cwd(...dir: string[])
 : (string) {
   return path.join(
       process.cwd(),
-      dir
+      ...dir
     );
 }
